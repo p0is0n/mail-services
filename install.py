fdae397d87a8b37d4cc2cd306f1a18dde0ff46cc
p0is0n <poisonoff@gmail.com>
2013-05-02 17:41:56 +0600

    small changes

--- install.py
+++ install.py
@@ -26,7 +26,6 @@ from twisted.internet.defer import fail, succeed
 from twisted.python.log import msg, err, startLogging
 from twisted.internet import reactor
 
-from core.db import sqlite
 from core.dirs import tmp
 from core.constants import DEBUG, CHARSET
 
@@ -35,52 +34,6 @@ startLogging(sys.stdout)
 # Install callable
 @inlineCallbacks
 def _(*args):
-	# Remove tables
-	deferreds = []
-	deferreds.append(sqlite.runOperation("""DROP TABLE IF EXISTS queue_messages"""))
-	deferreds.append(sqlite.runOperation("""DROP TABLE IF EXISTS queue_to"""))
-	deferreds.append(sqlite.runOperation("""DROP TABLE IF EXISTS queue_groups"""))
-
-	msg((yield DeferredList(deferreds)))
-
-	# Create tables
-	msg((yield sqlite.runOperation("""
-		CREATE TABLE queue_messages(
-			subject TEXT NOT NULL,
-			text TEXT,
-			html TEXT,
-			create_time INTEGER NOT NULL,
-			params TEXT NOT NULL
-		)
-	""")))
-
-	msg((yield sqlite.runOperation("""CREATE INDEX create_time_to_m ON queue_messages (create_time ASC)""")))
-
-	msg((yield sqlite.runOperation("""
-		CREATE TABLE queue_to(
-			message_id INTEGER NOT NULL,
-			group_id INTEGER,
-			email TEXT NOT NULL,
-			name TEXT NOT NULL,
-			create_time INTEGER NOT NULL,
-			parts TEXT NOT NULL,
-			priority INTEGER NOT NULL DEFAULT 0
-		)
-	""")))
-
-	msg((yield sqlite.runOperation("""CREATE INDEX message_to_i ON queue_to (message_id ASC)""")))
-	msg((yield sqlite.runOperation("""CREATE INDEX group_to_i ON queue_to (group_id ASC)""")))
-	msg((yield sqlite.runOperation("""CREATE INDEX create_time_to_i ON queue_to (create_time ASC)""")))
-
-	msg((yield sqlite.runOperation("""
-		CREATE TABLE queue_groups(
-			id INTEGER PRIMARY KEY ASC,
-			wait INTEGER NOT NULL DEFAULT 0,
-			sent INTEGER NOT NULL DEFAULT 0,
-			errors INTEGER NOT NULL DEFAULT 0
-		)
-	""")))
-
 	# Success
 	reactor.stop()
 
